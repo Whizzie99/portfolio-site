63095aa9e1e917446829b7f0c62830230e8e8a86
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-04-21 05:14:08 +0100

    added animation to home component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,20 +1,48 @@
 import { Link } from 'react-router-dom';
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 
 const Socials = () => {
+
+    const ref1 = useRef(null);
+    const ref2 = useRef(null);
+    const ref3 = useRef(null);
+
+    useEffect(() => {
+        const gitHub = ref1.current;
+        const linkedIn = ref2.current;
+        const twitter = ref3.current;
+
+        gsap.from([gitHub, linkedIn, twitter], 1, {
+            delay: 1,
+            ease: "Bounce.easeOut",
+            opacity: 0,
+            y: 65,
+            rotate: 360,
+            stagger:{
+                amount: 0.18
+            }
+
+        })
+
+
+
+    }, [ref1, ref2, ref3]);
+
     return ( 
         <ul>
-            <li className="link-item">
+            <li className="link-item" ref={ref1}>
                 <Link to={{ pathname: "https://github.com/whizzie99" }} target="_blank">
                     <i className="fab fa-github"></i>
                 </Link>
             </li>
-            <li className="link-item">
+            <li className="link-item" ref={ref2}>
                 <Link to={{ pathname: "https://www.linkedin.com/in/wisdom-obioma-244191208/" }} target="_blank">
                     <i className="fab fa-linkedin"></i>
                 </Link>
             </li>
-            <li className="link-item">
+            <li className="link-item" ref={ref3}>
                 <Link to={{ pathname: "https://twitter.com/_whizzie" }} target="_blank">
                     <i className="fab fa-twitter"></i>
                 </Link>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,51 @@
 import { Link } from 'react-router-dom';
+import { gsap } from 'gsap';
+
 import hello from '../assets/imgs/hello_hand.png';
 import Socials from '../components/Socials';
+import { useEffect, useRef } from 'react';
 
 const Home = () => {
+
+    const homeLine1 = useRef(null);
+    const homeLine2 = useRef(null);
+    const homeLine3 = useRef(null);
+    const handRef = useRef(null);
+
+    useEffect(() =>{
+
+        const el1 = homeLine1.current;
+        const el2 = homeLine2.current;
+        const el3 = homeLine3.current;
+        const handImg = handRef.current;
+
+        gsap.from([el1, el2, el3], 1, {
+            delay: 1,
+            ease: "power3.out",
+            x: -20,
+            opacity: 0,
+            stagger: {
+                amount: 0.18
+            }
+        });
+
+        gsap.from(handImg, 1.5, {
+            delay: 1.5,
+            ease: "Bounce.easeOut",
+            y: 50,
+            opacity: 0
+        });
+    }, [homeLine1, homeLine2, homeLine3, handRef]);
+
+
     return (
         <section className="home-content">
             <div className="left-section">
                 <div className="left-section-content">
                     <div className="container">
-                        <h2>Hi,</h2>
-                        <h4>Welcome to my space!</h4>
-                        <p>I'm wisdom obioma, a frontend developer.I build accessible, optimised and responsive websites and web apps.
+                        <h2 ref={homeLine1}>Hi,</h2>
+                        <h4 ref={homeLine2}>Welcome to my space!</h4>
+                        <p ref={homeLine3}>I'm wisdom obioma, a frontend developer.I build accessible, optimised and responsive websites and web apps.
                         </p>
                         <div className="social-links">
                             <Socials/>
@@ -22,7 +57,7 @@ const Home = () => {
                 </div>
             </div>
             <div className="right-section">
-                <div className="hand-img">
+                <div className="hand-img" ref={handRef}>
                     <img src={hello} alt="hello"/>
                 </div>
             </div>
