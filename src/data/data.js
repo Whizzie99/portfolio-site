142a7614bc3eacc044985e5bd3b72fb732960c07
wisdom obioma <wisdomobiomadev@gmail.com>
2022-07-12 20:24:56 +0100

    update projects list

--- src/data/data.js
+++ src/data/data.js
@@ -1,48 +1,69 @@
-import portfolio from '../assets/imgs/project_thumbnails/portfolio.png'
-import apex from '../assets/imgs/project_thumbnails/apex.png'
-import spacetagram from '../assets/imgs/project_thumbnails/spacetagram.png'
-import june from '../assets/imgs/project_thumbnails/june.png'
-import bioju from '../assets/imgs/project_thumbnails/bioju.png'
+import portfolio from '../assets/imgs/project_thumbnails/portfolio.png';
+import apex from '../assets/imgs/project_thumbnails/apex.png';
+import spacetagram from '../assets/imgs/project_thumbnails/spacetagram.png';
+import june from '../assets/imgs/project_thumbnails/june.png';
+import bioju from '../assets/imgs/project_thumbnails/bioju.png';
+import axgen from '../assets/imgs/project_thumbnails/axgen-shot.png';
+import reasons22 from '../assets/imgs/project_thumbnails/22-reasons.png';
 
 export const data = [
-    {
-        name: 'my portfolio site',
-        description: 'portfolio site to make basic information about me accessible to people and showcase my projects.',
-        thumbnail: portfolio,
-        tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
-        github: 'https://github.com/Whizzie99/portfolio-site',
-        preview: 'https://wisdomobioma.netlify.app'
-    },
-    {
-        name: 'apex site',
-        description: 'website for an imaginary firm',
-        thumbnail: apex,
-        tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
-        github: 'https://github.com/Whizzie99/apex-frontend',
-        preview: 'https://apexinvestments.netlify.app/'
-    },
-    {
-        name: 'spacetagram',
-        description: `a task for shopify internship. This project displays some pictures from Nasa's APOD api with the feature to like and unlike a photo`,
-        thumbnail: spacetagram,
-        tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
-        github: 'https://github.com/Whizzie99/spacetagram',
-        preview: 'https://spacetagram-test.netlify.app/'
-    },
-    {
-        name: `june's portfolio website`,
-        description: 'A portfolio site for a client',
-        thumbnail: june,
-        tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
-        github: 'https://github.com/Whizzie99/june_portfolio',
-        preview: 'https://june-test.netlify.app/'
-    },
-    {
-        name: 'biojus services website',
-        description: 'a business website to give customer easy access to her services',
-        tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
-        thumbnail: bioju,
-        github: 'https://github.com/Whizzie99/biojus-tech-frontend',
-        preview: 'https://biojuservices.netlify.app/'
-    }
-]
+	{
+		name: 'my portfolio site',
+		description:
+			'portfolio site to make basic information about me accessible to people and showcase my projects.',
+		thumbnail: portfolio,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/portfolio-site',
+		preview: 'https://wisdomobioma.netlify.app',
+	},
+	{
+		name: 'axgen site',
+		description: 'webiste for axgen nft',
+		thumbnail: axgen,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/axgen-client',
+		preview: 'https://axgen-test.netlify.app/',
+	},
+	{
+		name: '22 reasons why',
+		description:
+			'a website I built for someone really special to me..a simple way to let her know how much she means to me',
+		thumbnail: reasons22,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/22-reasons-why',
+		preview: 'https://22reasonswhy.netlify.app/',
+	},
+	{
+		name: 'apex site',
+		description: 'website for an imaginary firm',
+		thumbnail: apex,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/apex-frontend',
+		preview: 'https://apexinvestments.netlify.app/',
+	},
+	{
+		name: 'spacetagram',
+		description: `a task for shopify internship. This project displays some pictures from Nasa's APOD api with the feature to like and unlike a photo`,
+		thumbnail: spacetagram,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/spacetagram',
+		preview: 'https://spacetagram-test.netlify.app/',
+	},
+	{
+		name: `june's portfolio website`,
+		description: 'A portfolio site for a client',
+		thumbnail: june,
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		github: 'https://github.com/Whizzie99/june_portfolio',
+		preview: 'https://june-test.netlify.app/',
+	},
+	{
+		name: 'biojus services website',
+		description:
+			'a business website to give customer easy access to her services',
+		tools: ['fa-html5', 'fa-css3-alt', 'fa-js-square', 'fa-react'],
+		thumbnail: bioju,
+		github: 'https://github.com/Whizzie99/biojus-tech-frontend',
+		preview: 'https://biojuservices.netlify.app/',
+	},
+];
--- src/views/About.js
+++ src/views/About.js
@@ -1,72 +1,86 @@
+import { useEffect, useRef } from 'react';
 import Socials from '../components/Socials';
 import Tourbutton from '../components/Tourbutton';
 import { gsap } from 'gsap';
 import { Helmet } from 'react-helmet';
 
 import victoryHand from '../assets/imgs/victory_hand.png';
-import { useEffect, useRef } from 'react';
 
 const About = () => {
+	const ref1 = useRef(null);
+	const ref2 = useRef(null);
+	const ref3 = useRef(null);
 
-    const ref1 = useRef(null);
-    const ref2 = useRef(null);
-    const ref3 = useRef(null);
+	useEffect(() => {
+		const element1 = ref1.current;
+		const element2 = ref2.current;
+		const element3 = ref3.current;
 
-    useEffect(() => {
+		gsap.from(element1, 1, {
+			delay: 1,
+			ease: 'Elastic.easeOut',
+			opacity: 0,
+			x: -20,
+		});
 
-        const element1 = ref1.current;
-        const element2 = ref2.current;
-        const element3 = ref3.current;
+		gsap.from(element2, 1.5, {
+			delay: 1.5,
+			ease: 'power3.out',
+			x: -20,
+			opacity: 0,
+		});
 
-        gsap.from(element1, 1, {
-            delay: 1,
-            ease: "Elastic.easeOut",
-            opacity: 0,
-            x: -20
-        });
+		gsap.from(element3, 1.5, {
+			delay: 1.5,
+			ease: 'Bounce.easeOut',
+			y: 50,
+			opacity: 0,
+		});
+	}, []);
 
-        gsap.from(element2, 1.5, {
-            delay: 1.5,
-            ease: "power3.out",
-            x: -20,
-            opacity: 0,
-        });
+	return (
+		<section className="about-content">
+			<Helmet>
+				<title>Wisdom Obioma | About</title>
+			</Helmet>
+			<div className="left-section">
+				<div className="left-section-content">
+					<div className="container">
+						<h2 ref={ref1}>
+							About Me <span className="emoji">✌🏾</span>
+						</h2>
+						<p ref={ref2}>
+							Yeah!😎I'm frontend developer with over 2 years of experience
+							working with frontend web technologies, building responsive,
+							optimized and accessible interfaces that give users an amazing
+							experience.I listen to music a lot(even when working😅 )it helps
+							ease the mind. I play video games or soccer in my free time.
+							Technologies I work with include HTML, CSS, Javascript, React,
+							Bootstrap, Materialize css, css-in-js, strapi CMS, nodeJs. My
+							resume is available{' '}
+							<a
+								className="send-email-btn"
+								href="https://drive.google.com/file/d/1i1rOeVYB9G48ItKpenhgMRiHL3DCPYqC/view?usp=sharing"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Here
+							</a>
+						</p>
+						<div className="social-links">
+							<Socials />
+							<Tourbutton path="/projects" text="continue" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="right-section">
+				<div className="hand-img" ref={ref3}>
+					<img src={victoryHand} alt="hello" />
+				</div>
+			</div>
+		</section>
+	);
+};
 
-        gsap.from(element3, 1.5, {
-            delay: 1.5,
-            ease: "Bounce.easeOut",
-            y: 50,
-            opacity: 0
-        });
-
-    }, []);
-
-    return (
-        <section className="about-content">
-            <Helmet>
-                <title>Wisdom Obioma | About</title>
-            </Helmet>
-            <div className="left-section">
-                <div className="left-section-content">
-                    <div className="container">
-                        <h2 ref={ref1}>About Me <span className="emoji">✌🏾</span></h2>
-                        <p ref={ref2}>Yeah!😎I'm frontend developer with over 2 years of experience working with frontend web technologies, building responsive, optimized and accessible interfaces that give users an amazing experience.I listen to music a lot(even when working😅 )it helps ease the mind. I play video games or soccer in my free time.
-                        Technologies I work with include HTML, CSS, Javascript, React, Bootstrap, Materialize css, css-in-js. My resume is available <a className="send-email-btn" href="https://drive.google.com/file/d/1i1rOeVYB9G48ItKpenhgMRiHL3DCPYqC/view?usp=sharing" target="_blank" rel="noreferrer">Here</a>
-                        </p>
-                        <div className="social-links">
-                            <Socials/>
-                            <Tourbutton path="/projects" text="continue"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="right-section">
-                <div className="hand-img" ref={ref3}>
-                    <img src={victoryHand} alt="hello"/>
-                </div>
-            </div>
-        </section>
-    );
-}
- 
 export default About;
